8373d3a52ddcaa876a88899a10a8ccf768a9162c
Yeganathan S <hilariousengineer@protonmail.com>
2021-12-07 01:00:24 +0530

    Add Footer

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,25 @@ import Head from 'next/head'
 import Header from '../src/components/Header';
 import NavBar from '../src/components/NavBar';
 import ImageSlider from "../src/components/ImageSlider";
+import Footer from "../src/components/Footer";
 
 
 const Home = () => {
-  return (
-    <div className="md:w-100">
-      <Head>
-        <title>National Higher Secondary School, Irinjalakuda</title>
-        <meta name="description" content="National IJK School, Irinjalakuda" />
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-                integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-                crossOrigin="anonymous"/>
+    return (
+        <div className="md:w-100">
+            <Head>
+                <title>National Higher Secondary School, Irinjalakuda</title>
+                <meta name="description" content="National IJK School, Irinjalakuda"/>
+                <link rel="stylesheet" href={"https://pro.fontawesome.com/releases/v5.10.0/css/all.css"}
+                      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+                      crossOrigin="anonymous"/>
 
-          <link rel="icon" href="../public/favicons/home-96.png" />
-      </Head>
-        <NavBar/>
-        {/*<ImageSlider />*/}
-    </div>
-  )
+                <link rel="icon" href="../public/favicons/home-96.png"/>
+            </Head>
+            <NavBar/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default Home;
---
+++ src/components/Footer.js
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Syncrolaze
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+
+const footers = [
+    {
+        title: 'Company',
+        description: ['Team', 'History', 'Contact us', 'Locations'],
+    },
+    {
+        title: 'Features',
+        description: [
+            'Cool stuff',
+            'Random feature',
+            'Team feature',
+            'Developer stuff',
+            'Another one',
+        ],
+    },
+    {
+        title: 'Resources',
+        description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    },
+    {
+        title: 'Legal',
+        description: ['Privacy policy', 'Terms of use'],
+    },
+];
+
+
+const Footer = () => {
+
+
+    return (
+        <React.Fragment>
+            <GlobalStyles styles={{ul: {margin: 0, padding: 0, listStyle: 'none'}}}/>
+            <CssBaseline/>
+
+            {/* Footer */}
+            <Container
+                maxWidth="md"
+                component="footer"
+                sx={{
+                    borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+                    mt: 8,
+                    py: [3, 6],
+                }}
+
+            >
+                <Grid container spacing={4} justifyContent="space-evenly">
+                    {footers.map((footer) => (
+                        <Grid item xs={6} sm={3} key={footer.title}>
+                            <Typography variant="h6" color="text.primary" gutterBottom>
+                                {footer.title}
+                            </Typography>
+                            <ul>
+                                {footer.description.map((item) => (
+                                    <li key={item}>
+                                        <Link href="#" variant="subtitle1" underline="none" color="#3399ff" fontWeight="700">
+                                            {item}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Grid>
+                    ))}
+                </Grid>
+                <Copyright sx={{mt: 5}}/>
+            </Container>
+            {/* End footer */}
+        </React.Fragment>
+    );
+}
+
+export default Footer;
