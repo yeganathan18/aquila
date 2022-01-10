06c8276d28244f04199ed602dd29d96268833c5c
Yeganathan S <hilariousengineer@protonmail.com>
2022-01-10 21:04:12 +0530

    Support for fontawesome

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link
+            href={
+              'https://cdn.staticaly.com/gh/hung1001/font-awesome-pro/8af0edd/css/all.css'
+            }
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- src/pages/gallery/[slug].tsx
+++ src/pages/gallery/[slug].tsx
@@ -5,13 +5,17 @@ import Custom404 from '../404';
 
 const Event = () => {
   const router = useRouter();
+  // eslint-disable-next-line no-console
+  console.log(router, 'routes');
   if (!router.isFallback) {
     return <Custom404 />;
   }
+
   return (
     // eslint-disable-next-line react/jsx-no-undef
     <Section>
       <h1>Gallery</h1>
+      {router.query.slug}
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
         doloremque, quibusdam quisquam, quidem quisquam quisquam quisquam
