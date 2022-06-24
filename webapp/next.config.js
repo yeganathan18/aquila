63cb559955abb418ef156dc0885c2e01b3c16cca
Yeganathan S <hilariousengineer@protonmail.com>
2022-06-24 21:10:16 +0530

    add support for SWC and security headers

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,22 +1,54 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const ContentSecurityPolicy = `
+//   default-src 'self';
+//   script-src 'self';
+//   child-src example.com;
+//   style-src 'self' example.com;
+//   font-src 'self';  
+// `
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
-    // eslint-disable-next-line no-param-reassign
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    };
-    return config;
+const securityHeaders = [{
+  key: 'X-DNS-Prefetch-Control',
+  value: 'on'
+},
+{
+  key: 'Strict-Transport-Security',
+  value: 'max-age=63072000; includeSubDomains; preload'
+},
+{
+  key: 'X-XSS-Protection',
+  value: '1; mode=block'
+},
+{
+  key: 'X-Content-Type-Options',
+  value: 'nosniff'
+},
+{
+  key: 'Referrer-Policy',
+  value: 'origin-when-cross-origin'
+},
+// {
+//   key: 'Content-Security-Policy',
+//   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+// }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
-});
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
+}
