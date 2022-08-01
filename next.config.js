8d76841d91e923d921c567ef8f254fdde68a0372
Yeganathan S <hilariousengineer@protonmail.com>
2022-08-01 22:40:00 +0530

    chore: remove unnecessary configs for nextjs and unused modules
    I have switched to yarn rather than using npm, it's pretty much slow than yarn. Also most of the configs for the project seems to be heavy and not used so cleaned those.

--- next.config.js
+++ next.config.js
@@ -6,42 +6,7 @@
 //   font-src 'self';  
 // `
 
-const securityHeaders = [{
-  key: 'X-DNS-Prefetch-Control',
-  value: 'on'
-},
-{
-  key: 'Strict-Transport-Security',
-  value: 'max-age=63072000; includeSubDomains; preload'
-},
-{
-  key: 'X-XSS-Protection',
-  value: '1; mode=block'
-},
-{
-  key: 'X-Content-Type-Options',
-  value: 'nosniff'
-},
-{
-  key: 'Referrer-Policy',
-  value: 'origin-when-cross-origin'
-},
-// {
-//   key: 'Content-Security-Policy',
-//   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-// }
-]
-
 module.exports = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: securityHeaders,
-      },
-    ]
-  },
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
@@ -50,13 +15,6 @@ module.exports = {
     removeConsole: {
       exclude: ['error'],
     },
-  },
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    }
-    return config
   },
   output: 'standalone',
 }
